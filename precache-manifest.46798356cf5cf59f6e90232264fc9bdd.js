2cf7ad997b40fc3952dc5559df679ddef18947dc
aleksej <al1111997@yandex.ru>
2020-04-26 18:36:11 +0500

    Updates

---
+++ precache-manifest.46798356cf5cf59f6e90232264fc9bdd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "551411ceb1a6c957d0bba2651602469c",
+    "url": "/obs-subs/index.html"
+  },
+  {
+    "revision": "13d33eec626f9852b2b9",
+    "url": "/obs-subs/static/css/main.9e4cc40c.chunk.css"
+  },
+  {
+    "revision": "044beb3d92623e29581e",
+    "url": "/obs-subs/static/js/2.9390b46b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/obs-subs/static/js/2.9390b46b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13d33eec626f9852b2b9",
+    "url": "/obs-subs/static/js/main.93dae7fe.chunk.js"
+  },
+  {
+    "revision": "9d6d00b42218d1b26600",
+    "url": "/obs-subs/static/js/runtime-main.d5e64403.js"
+  },
+  {
+    "revision": "c16874a16976e162205d7bb02868aaa5",
+    "url": "/obs-subs/static/media/HelveticaRegular.c16874a1.otf"
+  },
+  {
+    "revision": "e2106d667f15cf5b332174d058a9de08",
+    "url": "/obs-subs/static/media/youtube-2.e2106d66.png"
+  },
+  {
+    "revision": "43bdd206621c9ca07e8bbb13f63757fd",
+    "url": "/obs-subs/static/media/youtube-3.43bdd206.png"
+  }
+]);
