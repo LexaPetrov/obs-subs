2b4d0fce1adfab68b00e1afac7b7051c15a64c5f
aleksej <al1111997@yandex.ru>
2020-04-26 17:54:31 +0500

    build

---
+++ build/precache-manifest.a891aef40b3bdd7a3953dad8eeed66d6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e92b0e19a112faa516448a6c56d6e6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f87d13f7c0a82336d6c",
+    "url": "/static/css/main.0391acbf.chunk.css"
+  },
+  {
+    "revision": "044beb3d92623e29581e",
+    "url": "/static/js/2.9390b46b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9390b46b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f87d13f7c0a82336d6c",
+    "url": "/static/js/main.ae61c90f.chunk.js"
+  },
+  {
+    "revision": "c7a70214fd655a9c9085",
+    "url": "/static/js/runtime-main.5a506d7f.js"
+  },
+  {
+    "revision": "c16874a16976e162205d7bb02868aaa5",
+    "url": "/static/media/HelveticaRegular.c16874a1.otf"
+  },
+  {
+    "revision": "e2106d667f15cf5b332174d058a9de08",
+    "url": "/static/media/youtube-2.e2106d66.png"
+  },
+  {
+    "revision": "43bdd206621c9ca07e8bbb13f63757fd",
+    "url": "/static/media/youtube-3.43bdd206.png"
+  }
+]);
